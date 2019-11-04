355186f62add4267f8f830a882883818eb07bc66
ylive001 <56458697+ylive001@users.noreply.github.com>
2019-11-03 19:41:06 -0800

    Update count_up.py

--- count_up.py
+++ count_up.py
@@ -6,7 +6,7 @@
 gff="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.37.gff3.gz"
 fasta="Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz"
 
-import os,gzip,itertools,csv,re
+import re, csv,os,gzip,itertools
 
 # this is code which will parse FASTA files
 # define what a header looks like in FASTA format
@@ -31,13 +31,32 @@ if not os.path.exists(gff):
 
 if not os.path.exists(fasta):
     os.system("curl -O ftp://ftp.ensemblgenomes.org/pub/bacteria/release-45/fasta/bacteria_0_collection/escherichia_coli_str_k_12_substr_mg1655/dna/Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.dna.chromosome.Chromosome.fa.gz")
+
     
 with gzip.open(gff,"rt") as fh:
-    # now add code to process this
-    gff = csv.reader(fh,delimiter="\t")
-    for row in gff:
-#        print(row)
-        print(row[3],row[6])
-        m = re.match('Chr',row[0])
-        if m:
-            print("line matches",row[0])
+    file = csv.reader (fh, delimiter= '\t')
+
+    gene_count = 0
+    total_gene_length = 0
+    for row in file:
+#        print (row)    
+        if row[0].startswith ("#"):
+            continue
+        gene = re.match (r'gene', row[2])
+        if gene:
+            gene_count += 1
+            gene_length = int(row[4]) - int(row[3])
+            total_gene_length += gene_length    
+    print ("number of genes is", gene_count)
+    print ("total length of the genes is", total_gene_length)
+
+
+with gzip.open(fasta,"rt") as f:
+    seqs = dict(aspairs(f))
+    
+    for k,v in seqs.items():
+        genome_length = len(v)
+        print("total length of genome is", genome_length)
+        
+    print("the percentage of them genome which is coding is", total_gene_length/genome_length*100)
+
